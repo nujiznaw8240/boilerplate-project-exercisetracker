7907c8b70700c0bd2afe551a4b2681456dd0a580
nujiznaw8240 <nujiznaw8240@gmail.com>
2025-03-01 16:07:56 +0100

    project exercise tracker

--- index.js
+++ index.js
@@ -1,16 +1,107 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const User = require('./models/User')
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/api/users', async function(req, res) {
+  const username = req.body.username;
+  const newUser = new User({username: username});
+  try{
+    await newUser.save();
+    return res.json({username: newUser.username, _id: newUser._id});
+  } catch (err) {
+    console.log(err);
+    return res.status(404).send({ message: 'error creating user' });
+  }
+})
+
+//67c31e0a5733ef8237b4ad8b
+
+app.post('/api/users/:_id/exercises', async function(req, res) {
+  try{
+    //prepare the data
+    const id = req.params._id;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const dateObj = new Date(req.body.date);
 
 
+    //find the user
+    let user = await User.findById(id);
+    if (!user) {
+      return res.status(404).send({ message: 'id not found' });
+    }
 
+    //update the user
+    user.log.push({
+      description: description,
+      duration: duration,
+      date: dateObj
+    });
+    await user.save();
+
+    //return json obj
+    return res.json({
+      username: user.username,
+      description: description,
+      duration: duration,
+      date: dateObj.toDateString(),
+      _id: user._id
+    });
+  } catch (err) {
+    return res.status(404).send({ message: 'update went wrong' });
+  }
+})
+
+app.get('/api/users/:_id/logs', async function(req, res) {
+  try{
+  //all the params
+    const id = req.params._id;
+    const fromD = req.query.from;
+    const toD = req.query.to;
+    const limit = req.query.limit;
+
+    //find the user
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).send({ message: 'id not found' });
+    }
+
+    let logs = user.log;
+    if (fromD || toD) {
+      const fromDate = fromD ? new Date(fromD) : new Date(0);
+      const toDate = toD ? new Date(toD) : new Date();
+      logs = logs.filter((log) => {
+        const logDate = log.date;
+        return logDate >= fromDate && logDate <= toDate;
+      });
+    }
+    if (limit) {
+      logs = logs.slice(0, Number(limit));
+    }
+
+    return res.json({
+      username: user.username,
+      count: logs.length,
+      _id: id,
+      log: logs.map((log) => ({
+        description: log.description,
+        duration: log.duration,
+        date: log.date.toDateString()
+      }))
+    });
+  } catch (err) {
+    return res.status(404).send({ message: 'get went wrong' });
+  }
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+const Schema = mongoose.Schema;
+
+const logEntrySchema = new Schema({
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true }, // You can also use `Date` type if you prefer
+});
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    log: [logEntrySchema], // Array of log entries
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
